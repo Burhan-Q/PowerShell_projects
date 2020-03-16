efae1aba05e6ade9272276fdb6e5655c1dbe46fe
Burhan <62214284+Burhan-Q@users.noreply.github.com>
2020-03-16 14:56:41 -0400

    Create sequential_numbering_images.ps1
    Use to give images a sequentially padded name. Replace the text 'BaseName' with a relevant name for the image set.

---
+++ sequential_numbering_images.ps1
@@ -0,0 +1,21 @@
+#search for all image types inside of current directory
+$imgs = Get-ChildItem *.jpg,*.png,*.gif,*.jpeg,*.bmp,*.tiff,*.tif
+
+#initialize counter for index
+$k = 0
+
+#loop over each image, change 'BaseName' text to name applicable to image set
+foreach($i in $imgs)
+{
+    #get file extension and save for later use
+    $ext = $i.Extension
+    
+    #create new item name and add padded number
+    $newname = ('BaseName' + '-' + ("{0:000}" -f $k) + $ext)
+    
+    #rename each item
+    Rename-Item $i -NewName $newname
+    
+    #increase counter index
+    $k++
+}
